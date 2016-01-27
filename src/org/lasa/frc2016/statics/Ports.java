260442b38dbfb359087f1d0815dbad57a0c8b2e0
James <james.asghar@gmail.com>
2016-01-27 09:54:19 -0600

    Fixed a few issues with syntax

--- src/org/lasa/frc2016/statics/Ports.java
+++ src/org/lasa/frc2016/statics/Ports.java
@@ -1,25 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.lasa.frc2016.statics;
 
-/**
- *
- * @author LASA Robotics
- */
 public class Ports {
 
     public static String AXIS_CAMERA_IP = "10.4.18.10";
 
     // PWM Ports
-    public static int LEFT_FRONT_MOTOR = 0;
-    public static int LEFT_BACK_MOTOR = 1;
+    public static int LEFT_FRONT_MOTOR = 1;
+    public static int LEFT_BACK_MOTOR = 0;
     public static int RIGHT_FRONT_MOTOR = 2;
     public static int RIGHT_BACK_MOTOR = 3;
     public static int INTAKE_MOTOR = 4;
     public static int FLY_WHEEL_MOTOR = 5;
+    public static int INTAKE_BUMP_SWITCH = 6;
 
     // Analog IO Ports
     
--- src/org/lasa/frc2016/subsystem/Sensor.java
+++ src/org/lasa/frc2016/subsystem/Sensor.java
@@ -5,10 +5,23 @@
  */
 package org.lasa.frc2016.subsystem;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import org.lasa.frc2016.statics.Ports;
+
 /**
  *
  * @author 418
  */
 public class Sensor {
+    public static Sensor instance;
     
+    public static DigitalInput intakeSwitch;
+    
+    private Sensor() {
+        intakeSwitch = new DigitalInput(Ports.INTAKE_BUMP_SWITCH);
+    }
+    
+    public static Sensor getInstance() {
+        return (instance == null) ? instance = new Sensor() : instance;
+    }
 }
