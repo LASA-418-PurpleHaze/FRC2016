9684580602e54640f4450c895b8e8041260baae3
James <james.asghar@gmail.com>
2016-01-28 15:42:22 -0600

    Fixed Ports

--- src/org/lasa/frc2016/statics/Ports.java
+++ src/org/lasa/frc2016/statics/Ports.java
@@ -5,13 +5,13 @@ public class Ports {
     public static String AXIS_CAMERA_IP = "10.4.18.10";
 
     // PWM Ports
-    public static int LEFT_FRONT_MOTOR = 1;
-    public static int LEFT_BACK_MOTOR = 0;
-    public static int RIGHT_FRONT_MOTOR = 2;
-    public static int RIGHT_BACK_MOTOR = 3;
-    public static int INTAKE_MOTOR = 4;
-    public static int FLYWHEEL_MASTER_MOTOR = 5;
-    public static int FLYWHEEL_SLAVE_MOTOR = 6;
+    public static final int LEFT_FRONT_MOTOR = 2;
+    public static final int LEFT_BACK_MOTOR = 1;
+    public static final int RIGHT_FRONT_MOTOR = 4;
+    public static final int RIGHT_BACK_MOTOR = 3;
+    public static final int INTAKE_MOTOR = 0;
+    public static final int FLYWHEEL_MASTER_MOTOR = 5;
+    public static final int FLYWHEEL_SLAVE_MOTOR = 6;
     
     // Encoder Ports
     public static final int LEFT_SIDE_A_ENCODER = 0;
