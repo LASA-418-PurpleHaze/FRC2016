835d44a54d20291475094fc8ecf473c731387491
Gralvarek <james.asghar@gmail.com>
2016-01-29 13:02:01 -0600

    I'm no longer dumb, removed the instances from the abstract class

--- src/org/lasa/lib/HazySubsystem.java
+++ src/org/lasa/lib/HazySubsystem.java
@@ -1,17 +1,10 @@
 package org.lasa.lib;
 
-import java.io.PrintWriter;
 import org.lasa.frc2016.input.DriverInput;
 import org.lasa.frc2016.input.SensorInput;
-import org.lasa.frc2016.subsystem.Drivetrain;
-import org.lasa.frc2016.subsystem.Flywheel;
-import org.lasa.frc2016.subsystem.Intake;
 
 public abstract class HazySubsystem {
     
-    protected Drivetrain drivetrain = Drivetrain.getInstance();
-    protected Intake intake = Intake.getInstance();
-    protected Flywheel flywheel = Flywheel.getInstance();
     protected DriverInput driverInput = DriverInput.getInstance();
     protected SensorInput sensorInput = SensorInput.getInstance();
 
