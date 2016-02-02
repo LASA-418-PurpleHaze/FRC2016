cd4ff02dbf2fbcb35e702ccbb89b95dc2d12f802
Gralvarek <james.asghar@gmail.com>
2016-02-02 16:19:57 -0600

    More fixing

--- src/org/lasa/frc2016/command/SpinUpShooter.java
+++ src/org/lasa/frc2016/command/SpinUpShooter.java
@@ -1,7 +1,7 @@
 package org.lasa.frc2016.command;
 
 import org.lasa.frc2016.statics.Constants;
-
+    
 public class SpinUpShooter extends HazyCommand {
 
     public SpinUpShooter(String nm, double t) {
