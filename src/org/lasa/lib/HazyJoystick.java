ff0eaf4f528d03fc67ec2e5113f5cdd2be7892a7
Gralvarek <james.asghar@gmail.com>
2016-02-07 20:24:34 -0600

    Joystick Wrapper includes all buttons

--- src/org/lasa/lib/HazyJoystick.java
+++ src/org/lasa/lib/HazyJoystick.java
@@ -28,8 +28,36 @@ public class HazyJoystick {
         return handleDeadBand(controller.getRawAxis(5));
     }
 
-    public boolean getButton(int button) {
-        return controller.getRawButton(button);
+    public boolean getA() {
+        return controller.getRawButton(1);
+    }
+    
+    public boolean getB() {
+        return controller.getRawButton(2);
+    }
+    
+    public boolean getX() {
+        return controller.getRawButton(3);
+    }
+    
+    public boolean getY() {
+        return controller.getRawButton(4);
+    }
+    
+    public boolean getLeftBumper() {
+        return controller.getRawButton(5);
+    }
+    
+    public boolean getRightBumper() {
+        return controller.getRawButton(6);
+    }
+    
+    public boolean getSelect() {
+        return controller.getRawButton(7);
+    }
+    
+    public boolean getStart() {
+        return controller.getRawButton(8);
     }
 
     private double handleDeadBand(double input) {
