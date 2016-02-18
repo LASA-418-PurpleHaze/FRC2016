414546c37cfb6729a06935d890ffeabd6e94dfd7
Gralvarek <james.asghar@gmail.com>
2016-02-17 20:45:07 -0600

    Forgot we need to turn off the intake after firing

--- src/org/lasa/frc2016/command/Shoot.java
+++ src/org/lasa/frc2016/command/Shoot.java
@@ -15,6 +15,12 @@ public class Shoot extends HazyCommand {
     }
 
     @Override
+    public void stop() {
+        super.stop();
+        intake.setMode(Intake.Mode.OFF);
+    }
+
+    @Override
     public void run() {
     }
 
