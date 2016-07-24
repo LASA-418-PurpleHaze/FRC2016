87fec2a26b7219aad53e88158e969a61bfff3365
Gijs Landwehr <gijslandwehr@utexas.edu>
2016-07-24 14:17:47 -0500

    Add wait command

---
+++ src/org/lasarobotics/frc2016/command/Wait.java
@@ -0,0 +1,28 @@
+package org.lasarobotics.frc2016.command;
+
+public class Wait extends Command {
+
+    public Wait(String name, double timeOut) {
+        super(name, timeOut);
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Start waiting for " + timeOut + " seconds.");
+    }
+
+    @Override
+    public boolean isDone() {
+        return isTimedOut();
+    }
+
+    @Override
+    public void run() {
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Finished waiting for " + timeOut + " seconds.");
+    }
+    
+}
